5e5898df5589dec87d8fd66e0b0774ef05999f9a
Marko Tadic <tadija@me.com>
2019-02-17 22:31:20 +0100

    Initial Commit

---
+++ SwiftPreview/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  SwiftPreview
+//
+//  Created by Marko Tadić on 2/17/19.
+//  Copyright © 2019 AE. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
